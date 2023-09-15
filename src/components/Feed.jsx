72e1eefdef3eb8cffafc8ad0337a3b0d7dea9328
Stacy Herold <telugumallastacy@gmail.com>
2023-09-15 14:23:16 +0530

    First Commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState, useEffect } from '@mui/icons-material';
+import { Box, Stack, Typography } from '@mui/icons-material';
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx:
+    "column", md: "row" } }}>
+      <Box sx={{ height: { sx:'auto', md:'92vh' }}}>
+
+      </Box>
+    </Stack>
+  )
+}
+
+export default Feed
